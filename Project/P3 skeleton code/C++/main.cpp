2463692c4a1dbc9d67fdf7fc6463e902cf813237
Liyao Gao <gao463@purdue.edu>
2019-02-24 22:28:32 -0500

    main file uploaded

--- Project/P3 skeleton code/C++/main.cpp
+++ Project/P3 skeleton code/C++/main.cpp
@@ -1,55 +1,192 @@
 #include <iostream>
-#include<string>
 #include<sstream>
 #include<fstream>
-#include<vector>
+
+#include "HashTable.hpp"
+#include "HeapPriorityQueue.hpp"
+
 using namespace std;
 
+ifstream inputfile;
+ofstream outputfile;
 
 void read_part_1()
 {
-	// TODO: Part1 : Create hash table
+   HashTable<int> table = HashTable<int>(2);
+   int lineNum = -1;
+   inputfile >> lineNum;
+   while (lineNum--) {
+      char c;
+      inputfile >> c;
+      // cout << "enter" << endl;
+      if (c == 'p') {
+         // cout << "Before put" << endl;
+         // table.print();
+         string k;
+         inputfile >> k;
+         int v;
+         inputfile >> v;
+         int r = table.put(k, v);
+         if (r == -100) {
+            cout << "null" << endl;
+         }
+         else {
+            cout << r << endl;
+         }
+         // cout << "After put" << endl;
+         // table.print();
+      }
+      if (c == 'g') {
+         string k;
+         inputfile >> k;
+         // cout << "get " << k << endl;
+         int r = table.get(k);
+         if (r == -100) {
+            cout << "null" << endl;
+         }
+         else {
+            cout << r << endl;
+         }
+      }
+      if (c == 'r') {
+         // cout << "Before replace" << endl;
+         // table.print();
+         string k;
+         inputfile >> k;
+         int v;
+         inputfile >> v;
+         int r = table.replace(k, v);
+         if (r == -100) {
+            cout << "null" << endl;
+         }
+         else {
+            cout << r << endl;
+         }
+         // cout << "After replace" << endl;
+         // table.print();
+      }
+      if (c == 's') {
+         cout << table.size() << endl;
+      }
+      if (c == 'c') {
+         string k;
+         inputfile >> k;
+         if (table.contains(k)) {
+            cout << 1 << endl;
+         }
+         else {
+            cout << 0 << endl;
+         }
+      }
+      if (c == 'm') {
+         int i;
+         inputfile >> i;
+         cout << table.getCollision(i) << endl;
+      }
+      if (c == 'd') {
+         string k;
+         inputfile >> k;
+         int r = table.remove(k);
+         if (r == -100) {
+            cout << "null" << endl;
+         }
+         else {
+            cout << r << endl;
+         }
+      }
+   }
 }
 
 void read_part_2()
 {
-	//TODO: Part 2: compute number of collisions
+   //TODO: Part 2: Anagrams
 }
 
 void read_part_3()
 {
-	//TODO: Part 3: create heap
+   HeapPriorityQueue<int> heap = HeapPriorityQueue<int>(0);
+   int lineNum = -1;
+   inputfile >> lineNum;
+   while (lineNum--) {
+      char c;
+      inputfile >> c;
+      // cout << "enter" << endl;
+      if (c == 'i') {
+         // cout << "Before put" << endl;
+         // table.print();
+         cout << "insert" << endl;
+         int v;
+         inputfile >> v;
+         heap.insert(v);
+         // cout << "After put" << endl;
+         // table.print();
+         heap.print();
+      }
+      if (c == 'r') {
+         int v;
+         inputfile >> v;
+         cout << "remove" << endl;
+         // cout << "get " << k << endl;
+         heap.removeMin();
+         heap.print();
+      }
+      if (c == 'm') {
+         cout << "get min" << endl;
+         int r = heap.min();
+         if (r == 0) {
+            cout << "empty" << endl;
+         }
+         else {
+            cout << r << endl;
+         }
+      }
+      if (c == 's') {
+         cout << "get sorted" << endl;
+         int size = heap.size();
+         int list[heap.size()];
+         int index = 0;
+         while (!heap.empty()) {
+            list[index] = heap.min();
+            heap.removeMin();
+            heap.print();
+            index ++;
+         }
+         for (int i = 0; i < size; i++) {
+            cout << list[i] << endl;
+         }
+      }
+   }
 }
 
 
 int main(int argc, char *argv[])
 {
-	// Read in the test case
-	ifstream inputfile;
-	ofstream outputfile;
-	
-	inputfile.open(argv[1]);
-	outputfile.open(argv[2]);
+   //Read in the test case
 
-	string str;
-	
-	getline(inputfile, str);
-	int part;
-	stringstream convert(str);
-	convert >> part; 
 
-	switch(part)
-	{
-		case 1:
-			read_part_1();
-			break;
-		case 2:
-			read_part_2();
-			break;
-		case 3:
-			read_part_3();
-			break;
-	}
+   inputfile.open(argv[1]);
+   outputfile.open(argv[2]);
+
+   string str;
+
+   getline(inputfile, str);
+   int part;
+   stringstream convert(str);
+   convert >> part;
+
+   switch(part)
+   {
+       case 1:
+           read_part_1();
+           break;
+       case 2:
+           read_part_1();
+           break;
+       case 3:
+           read_part_3();
+           break;
+   }
+
 	
 	return 0;
 }
